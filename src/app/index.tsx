49803364373ed2c7cafd863d028769c830acfb0c
lordxsumit <sum56it@gmail.com>
2026-03-29 13:13:45 +0530

    feat: add TextInput component for user input in Index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, StyleSheet, Button, Alert, Switch } from "react-native";
+import { View, StyleSheet, Button, Alert, Switch, TextInput } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,6 +10,7 @@ const styles = StyleSheet.create({
 });
 
 export default function Index() {
+  const [value, setValue] = useState("");
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
@@ -27,14 +28,25 @@ export default function Index() {
       value={isEnabled} 
       onValueChange={toggleSwitch}
       />
+
+      <TextInput
+      keyboardType="number-pad"
+      value={value}
+      onChangeText={setValue} 
+      style={{borderWidth: 1}}
+      multiline
+      numberOfLines={4}
+      placeholder="Enter your Text...."
+      />
     </View>
+
 
     // <ScrollView style={styles.container}>
     //     {
     //         [...Array(40)].map((_, i) => (
-    //             <Text>Hello from Home Screen {i+1} 🔥/</Text>
+    //             <Text key={i}>Hello from Home Screen {i+1} 🔥/</Text>
     //         ))
-    //     }
+    //     }  
     // </ScrollView>
   );
 }
